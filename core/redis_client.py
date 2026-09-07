b5b81e899886f47662605236e0992287ed0040f2
suhaanshakya99-spec <suhaanshakya99@gmail.com>
2026-09-07 08:04:33 +0545

    dockerized the project

--- core/redis_client.py
+++ core/redis_client.py
@@ -9,7 +9,7 @@ docker run -d \
   redis:latest
 '''
 
-redis_client = Redis(host="localhost", port=6379, db=0, decode_responses=True)
+redis_client = Redis(host="redis", port=6379, db=0, decode_responses=True)
 
 async def ratelimiting(email:str):
 
